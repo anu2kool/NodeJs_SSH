e567afe57459f1f6cc18fc45b9337c81ffecba25
anu2kool <anukooldwivedi@gmail.com>
2021-05-31 22:08:53 +0530

    Node js 4 first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+express=require('express');
+path=require('path');
+const bodyparser = require('body-parser');
+app=express();
+
+
+const PORT=3000||process.env.PORT;
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+app.get('/',(request,response)=>{
+	var filepath=path.join(path.resolve(),'index.html');
+	response.sendFile(filepath);
+});
+app.post('/',(request,response)=>{
+	var latitude=request.body.latitude;
+	var longitude=request.body.longitude;
+	console.log(`Latitude: ${latitude}`);
+	console.log(`Longitude: ${longitude}`);
+	var filepath=path.join(path.resolve(),'index.html');
+	response.sendFile(filepath);
+});
+
+app.listen(PORT,()=>{
+	console.log(`Server starting at ${PORT}`);
+});
